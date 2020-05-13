4458700417f3948f15f26ac85b634d95b9e054de
Cyberist-Edgar <201648748@qq.com>
2020-05-13 10:29:05 +0800

    回复时给用户一个通知信息

--- src/main/java/com/edgar/bbs/service/CommunityService.java
+++ src/main/java/com/edgar/bbs/service/CommunityService.java
@@ -1,6 +1,7 @@
 package com.edgar.bbs.service;
 
 import com.edgar.bbs.dao.ArticleDao;
+import com.edgar.bbs.dao.MessageDao;
 import com.edgar.bbs.dao.ReplyDao;
 import com.edgar.bbs.domain.Article;
 import com.edgar.bbs.domain.Reply;
@@ -18,21 +19,27 @@ public class CommunityService {
     @Resource
     private ArticleDao articleDao;
 
+    @Resource
+    private MessageDao messageDao;
+
+
     /*
     给对应的帖子回复
      */
-    public Result writeReplyToArticle(Long article_id, String reply, String username){
-        try{
+    public Result writeReplyToArticle(Long article_id, String reply, String username) {
+        try {
             Optional<Reply> r = replyDao.findReplyByArticleIdAndUsername(article_id, username);
-            if(r.isPresent()){
+            if (r.isPresent()) {
                 return new Result(400, "您对该帖子已经进行回答了");
             }
             Article article = articleDao.findById(article_id).get();
-            article.setComments(article.getComments()+1);
+            article.setComments(article.getComments() + 1);
             articleDao.saveAndFlush(article);
             replyDao.insertReply(article_id, reply, username);
+            String author = articleDao.getUsernameById(article_id);
+            messageDao.insert(article.getTitle(), String.format("%s评论了你的文章", username), author, "评论");
             return new Result(200, "回复成功");
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return new Result(400, "回复失败");
         }
@@ -42,16 +49,16 @@ public class CommunityService {
     删除对应的回答
      */
 
-    public Result deleteArticleById(Long article_id, String username){
-        Optional<Reply> reply = replyDao.findReplyByArticleIdAndUsername(article_id, username) ;
-        try{
-            if(reply.isPresent()){
+    public Result deleteArticleById(Long article_id, String username) {
+        Optional<Reply> reply = replyDao.findReplyByArticleIdAndUsername(article_id, username);
+        try {
+            if (reply.isPresent()) {
                 replyDao.deleteReplyByUsername(username);
                 return new Result(200, "删除成功");
-            }else{
+            } else {
                 return new Result(400, "您对该帖子并未进行回复");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return new Result(400, "删除失败");
         }
