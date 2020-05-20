7b446ff2e35c4b82b645b87aaff3b280c8574b36
Cyberist-Edgar <201648748@qq.com>
2020-05-20 20:47:49 +0800

    fix bug

--- src/main/java/com/edgar/bbs/dao/MessageDao.java
+++ src/main/java/com/edgar/bbs/dao/MessageDao.java
@@ -13,10 +13,10 @@ import java.util.Optional;
 @Transactional
 public interface MessageDao extends JpaRepository<Message, Long> {
 
-    @Query(value = "SELECT id, `type`, title,content, `time`, `read`, url FROM message WHERE username=?1 AND type=?2 OR type=?3 OR type=?4 ORDER BY time DESC ", nativeQuery = true)
+    @Query(value = "SELECT id, `type`, title,content, `time`, `read`, url FROM message WHERE username=?1 AND (type=?2 OR type=?3 OR type=?4) ORDER BY time DESC ", nativeQuery = true)
     List<MessageInfo> getAllByUsername(String username, String comment, String star, String like);
 
-    @Query(value = "SELECT COUNT(*) FROM message WHERE username=?1 AND `read`=0 AND type=?2 OR type=?3 OR type=?4", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM message WHERE username=?1 AND `read`=0 AND (type=?2 OR type=?3 OR type=?4)", nativeQuery = true)
     Long getUnreadMessage(String username, String comment, String star, String like);
 
     @Query(value = "SELECT * FROM message WHERE username=?1 AND id=?2", nativeQuery = true)
