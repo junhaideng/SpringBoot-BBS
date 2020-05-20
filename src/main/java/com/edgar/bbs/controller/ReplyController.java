ff530b7a3b74e4451edccd87937610bd52d3e6b1
Cyberist-Edgar <201648748@qq.com>
2020-05-20 20:39:38 +0800

    fix url

--- src/main/java/com/edgar/bbs/controller/ReplyController.java
+++ src/main/java/com/edgar/bbs/controller/ReplyController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
@@ -33,22 +32,22 @@ public class ReplyController {
 
     @ApiOperation("点赞回答")
     @RequestMapping(value = "/star", method = RequestMethod.POST)
-    public Result starReply(HttpSession session, @RequestParam("id") Long id, @RequestParam("type") String type, HttpServletRequest request){
+    public Result starReply(HttpSession session, @RequestParam("id") Long id, @RequestParam("type") String type, @RequestParam("url") String url) {
         String username = (String) session.getAttribute("username");
-        if(username==null){
+        if (username == null) {
             return new Result(400, "登录之后才能操作哦");
         }
         Optional<Reply> reply = replyDao.findById(id);
-        if(!reply.isPresent()){
+        if (!reply.isPresent()) {
             return new Result(400, "该回复不存在");
         }
         // 点赞数加一
-        if(type.equals("like")){
-            reply.get().setLike(reply.get().getLike()+1);
-            messageDao.insert(reply.get().getReply(),  String.format("%s点赞了你的回复", username), reply.get().getUsername(), "点赞", request.getRequestURL().toString());
-        }else if(type.equals("dislike")){
-            reply.get().setLike(reply.get().getLike()-1);
-        }else {
+        if (type.equals("like")) {
+            reply.get().setLike(reply.get().getLike() + 1);
+            messageDao.insert(reply.get().getReply(), String.format("%s点赞了你的回复", username), reply.get().getUsername(), "点赞", url);
+        } else if (type.equals("dislike")) {
+            reply.get().setLike(reply.get().getLike() - 1);
+        } else {
             return new Result(400, "参数不正确！");
         }
         replyDao.saveAndFlush(reply.get());
@@ -57,7 +56,7 @@ public class ReplyController {
 
     @ApiOperation("回答下的回复")
     @RequestMapping(value = "/comment", method = RequestMethod.POST)
-    public Result comment(HttpSession session, @RequestParam("comment") String comment, @RequestParam("reply_id") Long reply_id, @RequestParam("url") String url){
+    public Result comment(HttpSession session, @RequestParam("comment") String comment, @RequestParam("reply_id") Long reply_id, @RequestParam("url") String url) {
         return communityService.writeCommentToReply(session, comment, url, reply_id);
     }
 
