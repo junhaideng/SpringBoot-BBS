157211334f8b6854591a1ce03600433a534a59f2
Cyberist-Edgar <201648748@qq.com>
2020-05-20 19:38:38 +0800

    实现消息按设置显示

--- src/main/java/com/edgar/bbs/controller/UserController.java
+++ src/main/java/com/edgar/bbs/controller/UserController.java
@@ -9,21 +9,22 @@ import com.edgar.bbs.domain.Article;
 import com.edgar.bbs.domain.Avatar;
 import com.edgar.bbs.domain.Files;
 import com.edgar.bbs.service.AvatarService;
-import com.edgar.bbs.service.FileService;
 import com.edgar.bbs.service.UserService;
 import com.edgar.bbs.utils.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @Api(value = "用户相关")
@@ -48,6 +49,9 @@ public class UserController {
     private MessageDao messageDao;
 
     @Resource
+    private MessageSettingsDao messageSettingsDao;
+
+    @Resource
     private AvatarService avatarService;
 
     @Resource
@@ -65,7 +69,7 @@ public class UserController {
     @RequestMapping(value = "/loginlog", method = RequestMethod.POST)
     public List<LoginLogInfo> getUserLoginLog(HttpSession session) {
         String username = session.getAttribute("username").toString();
-        if(username ==null){
+        if (username == null) {
             return null;
         }
         return loginLogDao.findAllByUsername(username);
@@ -75,7 +79,7 @@ public class UserController {
     @RequestMapping(value = "/info", method = RequestMethod.POST)
     public UserInfo getUserInfo(HttpSession session) {
         String username = session.getAttribute("username").toString();
-        if(username ==null){
+        if (username == null) {
             return null;
         }
         UserInfo user = userDao.getInfoUsingUsername(username);
@@ -92,7 +96,7 @@ public class UserController {
     @RequestMapping(value = "/article", method = RequestMethod.POST)
     public List<Article> getUserArticles(HttpSession session) {
         String username = session.getAttribute("username").toString();
-        if(username ==null) {
+        if (username == null) {
             return null;
         }
         return articleDao.findArticlesByUsername(username);
@@ -100,7 +104,7 @@ public class UserController {
 
     @ApiOperation("删除帖子")
     @RequestMapping(value = "/article/delete", method = RequestMethod.POST)
-    public Result delelteArticle(@RequestParam("id") Long id, HttpSession session){
+    public Result delelteArticle(@RequestParam("id") Long id, HttpSession session) {
         return userService.deleteArticleById(id, session);
     }
 
@@ -108,7 +112,7 @@ public class UserController {
     @RequestMapping(value = "/files", method = RequestMethod.POST)
     public List<Files> getUserFiles(HttpSession session) {
         String username = session.getAttribute("username").toString();
-        if(username ==null){
+        if (username == null) {
             return null;
         }
         return filesDao.findAllByUsername(username);
@@ -118,13 +122,12 @@ public class UserController {
     @RequestMapping(value = "/delfiles", method = RequestMethod.POST)
     public Result delFiles(@RequestBody Long[] filesId, HttpSession session) {
         String username = session.getAttribute("username").toString();
-        if(username ==null){
+        if (username == null) {
             return new Result(400, "请先进行登录");
         }
         return userService.deleteFilesByIdAndUsername(filesId, username);
 
     }
-
 
 
     @ApiOperation(value = "修改用户的密码")
@@ -179,13 +182,29 @@ public class UserController {
     @ApiOperation("获取信息通知")
     @RequestMapping(value = "/message", method = RequestMethod.POST)
     public List<MessageInfo> getMessage(HttpSession session) {
-        return messageDao.getAllByUsername((String) session.getAttribute("username"));
+        String username = session.getAttribute("username").toString();
+        if (username == null) {
+            return null;
+        }
+        MessageSettingsInfo messageSettingsInfo = messageSettingsDao.findMessageSettingsUsingUsername(username);
+        String comment = messageSettingsInfo.isComment() ? "评论" : "comment"; // comment 实际上类似占空，无实意
+        String like = messageSettingsInfo.isLike() ? "点赞" : "like";
+        String star = messageSettingsInfo.isStar() ? "收藏" : "star";
+        return messageDao.getAllByUsername(username, comment, like, star);
     }
 
     @ApiOperation("获取未读信息总数")
     @RequestMapping(value = "/message/unread", method = RequestMethod.POST)
     public Map getUnreadMessage(HttpSession session) {
-        Long num = messageDao.getUnreadMessage((String) session.getAttribute("username"));
+        String username = session.getAttribute("username").toString();
+        if (username == null) {
+            return null;
+        }
+        MessageSettingsInfo messageSettingsInfo = messageSettingsDao.findMessageSettingsUsingUsername(username);
+        String comment = messageSettingsInfo.isComment() ? "评论" : "comment"; // comment 实际上类似占空，无实意
+        String like = messageSettingsInfo.isLike() ? "点赞" : "like";
+        String star = messageSettingsInfo.isStar() ? "收藏" : "star";
+        Long num = messageDao.getUnreadMessage(username, comment, like, star);
         HashMap<String, Long> map = new HashMap<>();
         map.put("unread", num);
         return map;
@@ -235,11 +254,11 @@ public class UserController {
     @RequestMapping("/avatar/get")
     public Result getAvatar(HttpSession session, HttpServletResponse response) throws IOException {
         String username = (String) session.getAttribute("username");
-        if(username==null){
+        if (username == null) {
             return new Result(400, "未登录无法获取用户头像");
-        }else{
+        } else {
             Optional<Avatar> avatar = avatarDao.getAvatarByUsername(username);
-             return avatarService.getAvatar(response,avatar);
+            return avatarService.getAvatar(response, avatar);
         }
     }
 
@@ -247,14 +266,14 @@ public class UserController {
     @RequestMapping("/avatar/get_by_username")
     public Result getAvatarByUsername(@RequestParam("username") String username, HttpServletResponse response) throws IOException {
         Optional<Avatar> avatar = avatarDao.getAvatarByUsername(username);
-        return avatarService.getAvatar(response,avatar);
+        return avatarService.getAvatar(response, avatar);
     }
 
     @ApiOperation("上传头像")
     @RequestMapping(value = "/avatar/upload", method = RequestMethod.POST)
-    public Result uploadAvatar(@RequestParam("file") MultipartFile file, HttpSession session){
+    public Result uploadAvatar(@RequestParam("file") MultipartFile file, HttpSession session) {
         String username = (String) session.getAttribute("username");
-        if(username == null){
+        if (username == null) {
             return new Result(400, "请登录");
         }
         return avatarService.uploadAvatar(file, username);
--- src/main/java/com/edgar/bbs/dao/MessageDao.java
+++ src/main/java/com/edgar/bbs/dao/MessageDao.java
@@ -13,11 +13,11 @@ import java.util.Optional;
 @Transactional
 public interface MessageDao extends JpaRepository<Message, Long> {
 
-    @Query(value = "SELECT id, `type`, title,content, `time`, `read`, url FROM message WHERE username=:username ORDER BY time DESC ", nativeQuery = true)
-    List<MessageInfo> getAllByUsername(String username);
+    @Query(value = "SELECT id, `type`, title,content, `time`, `read`, url FROM message WHERE username=?1 AND type=?2 OR type=?3 OR type=?4 ORDER BY time DESC ", nativeQuery = true)
+    List<MessageInfo> getAllByUsername(String username, String comment, String star, String like);
 
-    @Query(value = "SELECT COUNT(*) FROM message WHERE username=?1 AND `read`=0", nativeQuery = true)
-    Long getUnreadMessage(String username);
+    @Query(value = "SELECT COUNT(*) FROM message WHERE username=?1 AND `read`=0 AND type=?2 OR type=?3 OR type=?4", nativeQuery = true)
+    Long getUnreadMessage(String username, String comment, String star, String like);
 
     @Query(value = "SELECT * FROM message WHERE username=?1 AND id=?2", nativeQuery = true)
     Optional<Message> findMessageByUsernameAndId(String username, Long id);
